5a599786475e7a276de7919f691ae2e841521dca
مهرداد تابش <mehrdadit12@gmail.com>
2023-04-30 06:17:29 -0700

    ViewChildren

--- src/app/customer-info/customer-info.component.ts
+++ src/app/customer-info/customer-info.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {CustomerAddressComponent} from "../customer-address/customer-address.component";
 
 @Component({
@@ -10,12 +10,13 @@ export class CustomerInfoComponent implements OnInit{
 
   @ViewChild('firstName') firstName:any;
   @ViewChild('homeAddress') homeAddress:CustomerAddressComponent | undefined;
-
+  @ViewChildren(CustomerAddressComponent) addresses:QueryList<CustomerAddressComponent> | undefined;
   ngOnInit(): void {
   }
 
   save():void {
     console.log(this.firstName['nativeElement']?.value);
     console.log(this.homeAddress?.homeAddress);
+    this.addresses?.forEach(add=> console.log(add.homeAddress))
   }
 }
