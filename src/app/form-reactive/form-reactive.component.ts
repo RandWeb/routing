30b1c7d7d933f6d9493a00e45670ea6945ba65b9
مهرداد تابش <mehrdadit12@gmail.com>
2023-05-01 05:02:18 -0700

    nested forms

--- src/app/form-reactive/form-reactive.component.ts
+++ src/app/form-reactive/form-reactive.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-form-reactive',
@@ -9,17 +9,39 @@ import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 export class FormReactiveComponent implements OnInit {
   customerForm: any;
 
-  constructor(private fb:FormBuilder) {
+  constructor(private fb: FormBuilder) {
   }
 
   ngOnInit(): void {
-    this.customerForm =  this.fb.group({
-      firstName:['mehrdad',[Validators.required, Validators.minLength(4)]],
-      lastName:['',[Validators.required]],
-      customerCode:[''],
-      email:['']
+    this.customerForm = this.fb.group({
+      firstName: ['mehrdad', [Validators.required, Validators.minLength(4)]],
+      lastName: ['', [Validators.required]],
+      customerCode: [''],
+      email: [''],
+      accountForm: this.fb.group({
+        accountTitle: ['account title', Validators.required],
+        accountNumber: [' number', Validators.required],
+        sheba: ['1', Validators.required]
+      }),
+      addressList: this.fb.array([this.initAddressForm()])
     });
-  }/*  ngOnInit(): void {
+  }
+
+  initAddressForm():FormGroup {
+    return this.fb.group({
+      type: [''],
+      address:['']
+    });
+  }
+
+  get accountInfoForm() {
+    return this.customerForm.get('accountForm')
+  }
+  get addressListForm() {
+    return this.customerForm.get('addressList') as FormArray;
+  }
+
+  /*  ngOnInit(): void {
     this.customerForm = new FormGroup({
       firstName: new FormControl('mehrdad', [Validators.required, Validators.minLength(4)]),
       lastName: new FormControl('', Validators.required),
@@ -30,5 +52,15 @@ export class FormReactiveComponent implements OnInit {
 
   save(): void {
     console.log(this.customerForm.value);
+  }
+
+  addNewAddress(event:MouseEvent) {
+    event.preventDefault();
+    this.addressListForm.push(this.initAddressForm())
+  }
+
+  removeAddress(event: MouseEvent,index:number) {
+    event.preventDefault();
+    this.addressListForm.removeAt(index)
   }
 }
